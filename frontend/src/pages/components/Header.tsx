3729eef8b426869b7fc7ddb585e1ca4be9f93e4a
Charlie Bartels <charlie.bartels@daveramsey.com>
2025-01-02 08:34:19 -0600

    add tailwind

---
+++ frontend/src/pages/components/Header.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "../../index.css";
+
+interface HeaderProps {
+    title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+    const links = [
+        { name: 'Home', path: '/' },
+        { name: 'About', path: '/about' },
+        { name: 'Contact', path: '/contact' },
+    ];
+
+    return (
+        <header className="bg-orange-500 text-black p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <h1 className="text-xl font-bold">{title}</h1>
+                <nav>
+                    <ul className="flex space-x-4">
+                        {links.map((link) => (
+                            <li key={link.name}>
+                                <a href={link.path} className="hover:underline">
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- frontend/src/pages/games/GameList.tsx
+++ frontend/src/pages/games/GameList.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../api';
-
-interface Game {
-    id: number;
-    title: string;
-    description: string;
-}
+import {Game} from "../../models/Game";
+import Header from "../components/Header";
+import '../../index.css'
 
 function GameList() {
     const [games, setGames] = useState<Game[]>([]);
@@ -18,17 +15,14 @@ function GameList() {
             .catch(error => {
                 console.error('Error fetching games:', error);
             });
-    }, []); // [] loads the component after the page loads
+    }, []);
 
     return (
         <div>
-        <h1>Game List</h1>
+            <Header title="Game List" />
             {games.map(game => (
                 <div key={game.id}>
                     <h2>{game.title} - {game.description}</h2>
-                    <ul>
-                        {/*<li>{game.reviews}</li>*/}
-                    </ul>
                 </div>
             ))}
         </div>
